d64691a7c6482d6a40e8cab70d9ab3c20f281c9c
elifkarci2006-code <elifkarci2006@gmail.com>
2026-05-10 22:13:16 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,36 @@
+export const PALETTE = {
+  coral: "#f65a45",
+  sage: "#9BC78F",
+  aqua: "#76BFBE",
+  teal: "#58ADAD",
+  slate: "#3C4A5F",
+  bg: "#F5F7F8",
+  white: "#FFFFFF",
+  text: "#233044",
+  softText: "#5D6B7F",
+  border: "#DDE5EA",
+  warm: "#FFF2EC",
+};
 
+export const categories = [
+  "Tümü",
+  "Kafe & Restoran",
+  "Kurye & Dağıtım",
+  "Depo & Lojistik",
+  "Temizlik",
+  "Etkinlik & Organizasyon",
+  "Satış & Mağaza",
+  "Ofis & Yardımcı İşler",
+  "İnşaat & Fiziksel İş",
+  "Freelance / Dijital",
+];
+
+export const types = [
+  "Tümü",
+  "Günlük",
+  "Saatlik",
+  "Part Time",
+];
+
+export const SHOPIER_FEATURED_LINK =
+  "https://shopier.com/46018405";
